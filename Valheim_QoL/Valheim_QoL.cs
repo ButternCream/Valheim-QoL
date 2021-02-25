b4d58cf79c7c1274fe9172a842c8c6decc15dcac
Scott Mitchell <mitchsco@sonoma.edu>
2021-02-25 14:52:37 -0800

    Hoe no longer uses stamina plus some debugging tools for myself

--- Valheim_QoL/Valheim_QoL.cs
+++ Valheim_QoL/Valheim_QoL.cs
@@ -1,7 +1,9 @@
 ﻿using HarmonyLib;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Threading;
 using UnityEngine;
 
@@ -13,6 +15,19 @@ public static class Utils
         FileLog.Log(obj.ToString());
         FileLog.FlushBuffer();
     }
+
+    public static void LogInstanceData(object __instance)
+    {
+        BindingFlags bindingFlags = BindingFlags.Public |
+                            BindingFlags.NonPublic |
+                            BindingFlags.Instance |
+                            BindingFlags.Static;
+        Log($"\n======== {__instance.GetType()} =========");
+        foreach (FieldInfo field in __instance.GetType().GetFields(bindingFlags))
+        {
+            Log($"{field.Name} -> {field.GetValue(__instance)}");
+        }
+    }
         
 }
 
@@ -21,6 +36,7 @@ namespace Valheim_QoL
 
     public class Valheim_QoL
     {
+
         public static void Main(string[] args)
         {
             new Thread(delegate ()
@@ -28,6 +44,7 @@ namespace Valheim_QoL
                 var harmony = new Harmony("com.valheim.qol");
                 //Harmony.DEBUG = true;
                 FileLog.Reset();
+
                 bool flag;
                 do
                 {
@@ -40,7 +57,6 @@ namespace Valheim_QoL
                 Utils.Log("Patched functions!");
             }).Start();
         }
-
     }
 
 
@@ -95,6 +111,44 @@ namespace Valheim_QoL
             __item.m_shared.m_attack.m_attackStamina = 20f;
         }
     }
+
+    // Log class variables for useful info as well as stack data to trace 
+    // function calls
+    //[HarmonyPatch(typeof(Player), "UseStamina")]
+    public static class LogInstances
+    {
+        static StackTrace stack = new StackTrace();
+        public static bool Prefix(object __instance)
+        {
+            foreach (StackFrame frame in stack.GetFrames())
+            {
+                Utils.Log(frame.GetMethod().Name);
+            }
+            Utils.LogInstanceData(__instance);
+            return true;
+        }
+    }
+
+    // No stamina when hoe'ing
+    [HarmonyPatch(typeof(Player), "UseStamina")]
+    public static class HoeStamina
+    {
+
+        public static bool Prefix(Player __instance, ref float v)
+        {
+            var __item = __instance.GetRightItem();
+            if (__item.m_dropPrefab.ToString().Contains("Hoe"))
+            {
+                v = 0f;
+            }
+            return true;
+        }
+
+    }
+
+
+
+
 
     [HarmonyPatch(typeof(global::Console), "InputText")]
     public static class InputText_Patch
